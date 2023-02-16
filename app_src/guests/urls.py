a40d5fc929be5868ab13c98bb8f8063b906cd3f1
João Vitor Antoniassi Segantin <segantin.jv@gmail.com>
2023-02-15 23:36:11 -0300

    html and css improvements

--- app_src/guests/urls.py
+++ app_src/guests/urls.py
@@ -1,7 +1,6 @@
-from django.conf import settings
 from django.urls import path
 from .views import landing_page
 
 urlpatterns = [
-        path('', landing_page, name=settings.GUEST_FIRST_PAGE)
+        path('', landing_page, name='landing_page')
 ]
